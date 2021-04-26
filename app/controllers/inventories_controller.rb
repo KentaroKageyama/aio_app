ce9b75a03b22a1ef27bc714d023f6f9c584d73b2
Taroon29 <k.taro.a01@gmail.com>
2021-04-26 14:51:25 +0900

    inventory削除機能実装

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,9 +1,27 @@
 class InventoriesController < ApplicationController
 
+  def edit
+  end
+
   def destroy
-    inventory = Inventory.find(params[:id])
-    inventory.destroy
+    @inventory = Inventory.find(params[:id])
+    change_stock
+    @inventory.destroy
     redirect_to stocks_path
+  end
+
+  private
+
+  def change_stock
+    stock = Stock.find(@inventory.stock_id)
+    binding.pry
+    if @inventory.in_out_id == 1
+      qty = stock.quantity - @inventory.inv_quantity
+      stock.update_attribute(:quantity, qty)
+    elsif inventory.in_out_id == 2
+      qty = stock.quantity + @inventory.inv_quantity
+      stock.update_attribute(:quantity, qty)
+    end
   end
 
 end
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -16,6 +16,7 @@ class StocksController < ApplicationController
       render action: :index
     end
   end
+  
 
   private
 
