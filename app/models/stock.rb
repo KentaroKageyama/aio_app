292b13b832ee608ff64e085a9856f9695c33209a
Taroon29 <k.taro.a01@gmail.com>
2021-04-27 14:38:30 +0900

    stockバリデーション

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,7 @@
 class Stock < ApplicationRecord
 
+  validates :quantity, presence: true, numericality: true
+
   belongs_to :item
   has_many :inventories, dependent: :destroy
   accepts_nested_attributes_for :inventories
