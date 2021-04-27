a9a11d542f963211c35eba6a99d16f203e37db61
Taroon29 <k.taro.a01@gmail.com>
2021-04-27 11:53:57 +0900

    userバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は英字と数字の両方を含めて設定してください' }
 
+  with_options presence: true do
+    validates :company
+    validates :phonenumber, numericality: true, length: { maximum: 11 }
+    validates :zip_code, numericality: true, length: { maximum: 7 } 
+    validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
+    validates :city, :address
+    validates :bank, :branch
+    validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
+    validates :bank_number, numericality: true, length: { maximum: 7 }
+    validates :bank_account
+  end
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :bank_type
