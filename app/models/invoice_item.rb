25ce34fb38771bf9df0188351d3c53dab9ebce2b
Taroon29 <k.taro.a01@gmail.com>
2021-04-27 14:35:34 +0900

    invoice_itemバリデーション

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,5 +1,12 @@
 class InvoiceItem < ApplicationRecord
 
+  with_options presence: true do
+    validates :chain_item
+    with_options numericality: true do
+      validates :price, :quantity
+    end
+  end
+
   belongs_to :invoice
   
 end
