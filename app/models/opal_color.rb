1ba8f49bd3e71dc74a7a9abf3219d766aa420b42
Taroon29 <k.taro.a01@gmail.com>
2021-04-27 14:17:24 +0900

    opal_colorバリデーション

--- app/models/opal_color.rb
+++ app/models/opal_color.rb
@@ -1,10 +1,10 @@
 class OpalColor < ApplicationRecord
+
+  validates :color, presence: true
   
   acts_as_list
 
   has_many :items
   has_many :glasses
-
-  validates :color, presence: true
 
 end
