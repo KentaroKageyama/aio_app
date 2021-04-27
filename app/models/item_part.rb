6896af0e5ac36f19b9298e69e4b313074ac04677
Taroon29 <k.taro.a01@gmail.com>
2021-04-27 12:37:19 +0900

    item_partバリデーション

--- app/models/item_part.rb
+++ app/models/item_part.rb
@@ -1,8 +1,9 @@
 class ItemPart < ApplicationRecord
+
+  validates :item_id, uniqueness: { scope: :part_id } # 複合一意制約
+  validates :quantity, presence: true, numericality: true
   
   belongs_to :item
   belongs_to :part
-
-  validates :item_id, uniqueness: { scope: :part_id } 
   
 end
