f3990547fc7a3e677b4bb4e9834afc8aced2d5ee
Taroon29 <k.taro.a01@gmail.com>
2021-04-27 12:40:06 +0900

    item_glassバリデーション

--- app/models/item_glass.rb
+++ app/models/item_glass.rb
@@ -1,8 +1,9 @@
 class ItemGlass < ApplicationRecord
 
+  validates :item_id, uniqueness: { scope: :glass_id } # 複合一意制約
+  validates :quantity, presence: true, numericality: true
+
   belongs_to :item
   belongs_to :glass
 
-  validates :item_id, uniqueness: { scope: :glass_id } 
-  
 end
