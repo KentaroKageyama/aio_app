62ccd52725b0b1a51df88058c2667d58b8a109a9
Taroon29 <k.taro.a01@gmail.com>
2021-04-27 14:27:19 +0900

    invoiceバリデーション

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,11 @@
 class Invoice < ApplicationRecord
 
+  with_options presence: true do
+    validates :issue_date
+    validates :issue_number, numericality: true
+    validates :content, :client_name, :client_percentage
+  end
+
   has_many :invoice_items, dependent: :destroy
   accepts_nested_attributes_for :invoice_items
 
