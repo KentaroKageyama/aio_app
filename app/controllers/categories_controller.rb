9ba2a422412f74af1c444ae219e290ba2a7ebe54
Taroon29 <k.taro.a01@gmail.com>
2021-05-17 21:01:40 +0900

    Update categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_category, only: [:edit, :update, :destroy]
 
   def create
     category = Category.new(category_params)
@@ -8,17 +9,14 @@ class CategoriesController < ApplicationController
   end
 
   def edit
-    @category = Category.find(params[:id])
   end
 
   def update
-    @category = Category.find(params[:id])
     @category.update(category_params)
     redirect_to new_other_items_path
   end
 
   def destroy
-    category = Category.find(params[:id])
     category.destroy
     redirect_to new_other_items_path
   end
@@ -34,4 +32,9 @@ class CategoriesController < ApplicationController
   def category_params
     params.require(:category).permit(:name)
   end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
 end
