1d2c28f2dbd4c15244b4dba9569c5687e7534471
Taroon29 <k.taro.a01@gmail.com>
2021-05-24 20:38:01 +0900

    editページに適用

--- app/javascript/input_price.js
+++ app/javascript/input_price.js
@@ -33,3 +33,39 @@ if (document.URL.match( /invoices\/new/ )){
     });
   });
 }
+
+if (document.URL.match( /invoices\/.+\/edit/ )){
+  document.addEventListener("DOMContentLoaded", () => {
+    const addItem = document.getElementById("add-item");
+    addItem.addEventListener("click", () =>{
+
+      setTimeout(
+        function () {
+          const items = document.querySelectorAll(".invoice-item-name");
+          const prices = document.querySelectorAll(".invoice-item-price");
+
+          items.forEach(function(item, index) {
+            item.addEventListener("change", ()=> {
+              if(item.value == ""){
+                return true;
+              }
+              const XHR = new XMLHttpRequest();
+              XHR.open("GET", `../../items/output_price/?name=${item.value}`, true);
+              XHR.responseType = "json";
+              XHR.send();
+              XHR.onload = () => {
+                if (XHR.status != 200) {
+                  return null;
+                }
+                console.log(XHR.response.price);
+                prices[index].value = XHR.response.price;
+              };
+            });
+          });
+
+        },"1"
+      );
+
+    });
+  });
+}
