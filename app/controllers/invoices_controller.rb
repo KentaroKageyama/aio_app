5b6b0c6c8053ec68853e370e6f1b5b06f62245b7
Taroon29 <k.taro.a01@gmail.com>
2021-05-24 20:38:37 +0900

    Update invoices_controller.rb

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,5 +1,6 @@
 class InvoicesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_invoice, only: [:edit, :update, :destroy]
 
   def index
     @invoices = Invoice.all.order(issue_date: :DESC)
@@ -16,18 +17,15 @@ class InvoicesController < ApplicationController
   end
 
   def edit
-    @invoice = Invoice.find(params[:id])
     @client = Client.find_by(name: @invoice.client_name)
   end
 
   def update
-    @invoice = Invoice.find(params[:id])
     @invoice.update(invoice_params)
     redirect_to post_pdf_index_path(format: "pdf", invoice_id: @invoice.id, client_id: params[:invoice][:client_id])
   end
 
   def destroy
-    @invoice = Invoice.find(params[:id])
     @invoice.destroy
     redirect_to action: :index
   end
@@ -37,6 +35,10 @@ class InvoicesController < ApplicationController
   def invoice_params
     client = Client.find(params[:invoice][:client_id]) if params[:invoice][:client_id].present?
     params.require(:invoice).permit(:issue_date, :issue_number, :content, invoice_items_attributes: [:id, :chain_item, :size, :quantity, :price, :_destroy]).merge(client_name: client.name, client_percentage: client.percentage)
+  end
+
+  def set_invoice
+    @invoice = Invoice.find(params[:id])
   end
 
 end
